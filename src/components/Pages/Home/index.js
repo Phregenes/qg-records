6c9a9a94d37a9afef08313ebbe7eef3c25c579dc
Pedro Regenes <phregenes@gmail.com>
2020-09-28 14:47:44 -0300

    first commit

---
+++ src/components/Pages/Home/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './styles.css';
+
+import logo from '../../../assents/img/logo.png';
+
+const Home = () => 
+
+<body>
+  <div className="pimg1">
+    <div className="ptext">
+      <img src={logo}/>
+    </div>
+  </div>
+
+  <section className="section section-1">
+    <h2 className="purple">Lorem Ipsum</h2>
+    <p>
+    Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+    </p>
+  </section>
+
+  <div className="pimg2">
+    <div className="ptext">
+      <span className="text-title">
+        Lorem Ipsum
+      </span>
+    </div>
+  </div>
+
+  <section className="section section-2">
+    <h2>Lorem Ipsum</h2>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat tortor metus, sit amet sollicitudin libero pulvinar eleifend. Vestibulum vehicula sed risus eu laoreet. Nunc mollis fringilla porttitor. Nulla facilisi. Nullam ultrices nec urna at pretium. Cras ac efficitur magna, vitae efficitur justo. Etiam lacus purus, scelerisque vel consequat nec, condimentum eget massa. Praesent erat leo, dictum eu pharetra in, lobortis eget velit. Aenean et aliquam enim, sed consectetur ex. Etiam purus nunc, interdum et placerat eu, ullamcorper eget urna. Donec ultricies eleifend elit non semper.
+    </p>
+  </section>
+
+  <div className="pimg3">
+    <div className="ptext">
+      <span className="text-title">
+      Lorem Ipsum
+      </span>
+    </div>
+  </div>
+
+  <section className="section section-3">
+    <h2>Lorem Ipsum</h2>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae eros sed erat pellentesque tristique vel quis nulla. Aenean dui sapien, eleifend nec enim ullamcorper, commodo consequat ante. Phasellus posuere posuere tincidunt. Cras ante velit, aliquet a vulputate ut, dapibus vitae elit. Maecenas maximus massa leo, eget hendrerit mauris viverra eu. Donec eu porttitor nibh. Mauris eu gravida diam. Maecenas interdum pharetra lectus, in finibus sem imperdiet quis. Nam faucibus tortor urna. Ut ac molestie libero. Praesent at tortor placerat, mattis nunc at, auctor enim. Curabitur congue leo id mauris eleifend, fringilla efficitur est posuere. Aenean rhoncus semper dictum. Aenean cursus dapibus metus. Aliquam quis ultrices libero. Curabitur rhoncus dolor ac sem porta, sed blandit ligula luctus.
+    </p>
+  </section>
+
+  <div className="pimg1">
+    <div className="ptext">
+      <span className="text-title">
+        Social Media
+      </span>
+    </div>
+  </div>
+</body>
+
+export default Home;
